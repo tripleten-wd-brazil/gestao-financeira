d0a1dfd0309e3807ec4d8ee46a7d5e21bc018f02
Thiago Puntar <thiagopuntar@gmail.com>
2022-06-22 21:14:47 -0300

    js 5

--- index.js
+++ index.js
@@ -7,8 +7,28 @@ function addListeners() {
 
 function save(e) {
   e.preventDefault();
+  const isValid = validateForm();
+  if (!isValid) {
+    return; // early return
+  }
   const { data, descricao, valor, dataInput } = getValuesFromForm();
 
+  addRow(data, descricao, valor);
+  const records = JSON.parse(localStorage.getItem("records")) || [];
+  records.push({
+    data,
+    descricao,
+    valor,
+  });
+  localStorage.setItem("records", JSON.stringify(records));
+
+  document.forms.lancamentos.reset();
+  sumUp();
+  dataInput.focus();
+  showAlert("Seu registro foi inserido com sucesso");
+}
+
+function addRow(data, descricao, valor) {
   const tbody = document.querySelector("#tbody");
   const row = document.querySelector("#template-row").content.cloneNode(true);
   const dataCell = row.querySelector(".data");
@@ -18,11 +38,17 @@ function save(e) {
   descricaoCell.textContent = descricao;
   valorCell.textContent = parseFloat(valor).toFixed(2);
   tbody.append(row);
-  document.forms.lancamentos.reset();
-  atualizaTotal();
-  dataInput.focus();
-  showAlert("Seu registro foi inserido com sucesso");
 }
+
+function loadData() {
+  const records = JSON.parse(localStorage.getItem("records")) || [];
+  records.forEach(({ data, descricao, valor }) =>
+    addRow(data, descricao, valor)
+  );
+  sumUp();
+}
+
+loadData();
 
 function getValuesFromForm() {
   const dataInput = document.querySelector("#data");
@@ -36,7 +62,23 @@ function getValuesFromForm() {
   return { data, descricao, valor, dataInput };
 }
 
-function atualizaTotal() {
+function validateForm() {
+  const isValidData = validateInput("data");
+  const isValidDescricao = validateInput("descricao", "Descrição obrigatória");
+  const isValidValor = validateInput("valor");
+
+  return isValidData && isValidDescricao && isValidValor;
+}
+
+function validateInput(inputName, errorMessage = "Campo Obrigatório") {
+  const input = document.querySelector(`#${inputName}`);
+  const divError = document.querySelector(`.form__error_${inputName}`);
+  const isValid = input.checkValidity();
+  divError.textContent = isValid ? "" : errorMessage;
+  return isValid;
+}
+
+function sumUp() {
   const tdsValor = document.querySelectorAll(".valor");
   const total = Array.from(tdsValor).reduce(
     (acc, tdValor) => (acc += parseFloat(tdValor.textContent)),
@@ -49,8 +91,12 @@ function atualizaTotal() {
 
 const deleteRow = (event) => {
   if (event.target.classList.contains("btn-delete")) {
+    const index = getIndexOfElementEvent(event);
     event.target.parentNode.parentNode.remove();
-    atualizaTotal();
+    const records = JSON.parse(localStorage.getItem("records"));
+    records.splice(index, 1);
+    localStorage.setItem("records", JSON.stringify(records));
+    sumUp();
     showAlert("Seu registro foi removido com sucesso", "danger");
   }
 };
@@ -64,6 +110,12 @@ function showAlert(message, type = "success") {
   setTimeout(() => {
     alertDiv.remove();
   }, 1000 * 3);
+}
+
+function getIndexOfElementEvent(evt) {
+  const elementOf = evt.target.closest("tr");
+  const arrayElements = Array.from(elementOf.parentElement.children);
+  return arrayElements.indexOf(elementOf);
 }
 
 addListeners();
